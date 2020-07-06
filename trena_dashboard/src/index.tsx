ff3e9d31ef401d66a9600de3746281c04b18a386
Walter José <walterjgsp@gmail.com>
2020-07-06 17:48:24 -0300

    Adding sentry to track error in the project and deployment

--- trena_dashboard/src/index.tsx
+++ trena_dashboard/src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import * as Sentry from '@sentry/react';
 import * as serviceWorker from './serviceWorker';
+import Config from "./config/Config";
+
+Sentry.init({dsn: Config.SENTRY_DNS, environment: Config.ENVIRONMENT});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
