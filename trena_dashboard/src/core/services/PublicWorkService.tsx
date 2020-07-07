0e34254ff2517d005033c9483168b62ff8e351fb
Walter José <walterjgsp@gmail.com>
2020-07-06 21:39:53 -0300

    Fix API endpoints

--- trena_dashboard/src/core/services/PublicWorkService.tsx
+++ trena_dashboard/src/core/services/PublicWorkService.tsx
@@ -4,7 +4,7 @@ import request from "superagent";
 
 export class PublicWorkService {
     static async loadPublicWorks(): Promise<PublicWork[]> {
-        const call = Config.BASE_URL + "/publicworks"
+        const call = Config.BASE_URL + "/publicworks/"
         return request.get(call)
             .then(res => {
                 let listPublicWorks: PublicWork[] = res.body
