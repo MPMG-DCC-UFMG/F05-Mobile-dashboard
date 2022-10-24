e36371962cdc7905d0696cf696a2ab2ebf150ca3
Filipe <filipeasm18@gmail.com>
2022-10-24 18:39:49 -0300

    Fix: Fixed config files and update dependencies

--- trena_dashboard/src/config/Config.tsx
+++ trena_dashboard/src/config/Config.tsx
@@ -1,6 +1,6 @@
 export default class Config {
-    static ENVIRONMENT?: string = window._env_.REACT_APP_ENVIRONMENT;
-    static BASE_URL?: string = "https://trena.gsi.mpmg.mp.br";
-    static SENTRY_DNS?: string = window._env_.REACT_APP_SENTRY_DNS;
-    static API_KEY: string = window._env_.REACT_APP_API_KEY ?? "";
+  static ENVIRONMENT?: string = process.env.REACT_APP_ENVIRONMENT;
+  static BASE_URL?: string = process.env.REACT_APP_BASE_URL;
+  static SENTRY_DNS?: string = process.env.REACT_APP_SENTRY_DNS;
+  static API_KEY?: string = process.env.REACT_APP_API_KEY;
 }
