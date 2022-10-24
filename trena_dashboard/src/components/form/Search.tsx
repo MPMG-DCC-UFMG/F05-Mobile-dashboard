0c54e701844fef91696f3c512e16b2d5b90137af
Marco Tulio Mendes Madeira <tuliomarco9704@hotmail.com>
2022-10-24 20:18:15 -0300

    Fix: Search and ItemActionMenu components

--- trena_dashboard/src/components/form/Search.tsx
+++ trena_dashboard/src/components/form/Search.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { InputAdornment, TextField } from "@mui/material";
+import { ManageSearch } from "@mui/icons-material";
 
 interface SearchProps {
-    onTextChanged: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onTextChanged: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export const Search: React.FC<SearchProps> = (props) => {
-    return (
-        <p className="control has-icons-left">
-            <input className="input" type="text" placeholder="Buscar" onChange={props.onTextChanged}/>
-            <span className="icon is-left">
-                <FontAwesomeIcon icon={faSearch}/>
-            </span>
-        </p>
-    )
+  return (
+    <TextField
+      fullWidth
+      size="small"
+      onChange={props.onTextChanged}
+      label="Tipo de Obra"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <ManageSearch/>
+          </InputAdornment>
+        ),
+      }}
+    />
+    // <p className="control has-icons-left">
+    //     <input className="input" type="text" placeholder="Buscar" onChange={props.onTextChanged}/>
+    //     <span className="icon is-left">
+    //         <FontAwesomeIcon icon={faSearch}/>
+    //     </span>
+    // </p>
+  )
 }
--- trena_dashboard/src/components/menus/ItemActionsMenu.tsx
+++ trena_dashboard/src/components/menus/ItemActionsMenu.tsx
@@ -1,37 +1,67 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlus, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Add, Edit, Delete } from "@mui/icons-material";
+import { Grid, ButtonGroup, Button, IconButton, Stack } from "@mui/material";
 
 interface ItemActionsMenuProps {
-    itemSelected: boolean,
-    onAddClicked: () => void,
-    onEditClicked: () => void,
-    onDeleteClicked: () => void
+  itemSelected: boolean,
+  onAddClicked: () => void,
+  onEditClicked: () => void,
+  onDeleteClicked: () => void,
 }
 
 export const ItemActionsMenu: React.FC<ItemActionsMenuProps> = (props) => {
-    const {itemSelected} = props
+  const { itemSelected } = props
 
-    return (
-        <div className="container">
-            <div className="buttons has-addons is-centered">
-                <button className="button is-success" onClick={props.onAddClicked}>
-                    <span className="icon is-small">
-                        <FontAwesomeIcon icon={faPlus}/>
-                    </span>
-                </button>
-                <button className="button is-info" onClick={props.onEditClicked} disabled={!itemSelected}>
-                    <span className="icon is-small">
-                        <FontAwesomeIcon icon={faEdit}/>
-                    </span>
-                </button>
-                <button className="button is-danger" onClick={props.onDeleteClicked} disabled={!itemSelected}>
-                    <span className="icon is-small">
-                        <FontAwesomeIcon icon={faTrash}/>
-                    </span>
-                </button>
-            </div>
-        </div>
+  return (
+    <Stack spacing={2} direction="row">
+        <Button
+          size="small"
+          onClick={props.onAddClicked}
+          color="success"
+          variant="contained"
+        >
+          <Add/>
+        </Button>
+        <Button
+          size="small"
+          onClick={props.onEditClicked}
+          disabled={!itemSelected}
+          color="info"
+          variant="contained"
+        >
+          <Edit />
+        </Button>
+        <Button
+          size="small"
+          onClick={props.onDeleteClicked}
+          disabled={!itemSelected}
+          color="error"
+          variant="contained"
+        >
+          <Delete />
+        </Button>
+        </Stack>
 
-    )
-}
+    // <div className="container">
+    //     <div className="buttons has-addons is-centered">
+    //         <button className="button is-success" onClick={props.onAddClicked}>
+    //             <span className="icon is-small">
+    //                 <FontAwesomeIcon icon={faPlus}/>
+    //             </span>
+    //         </button>
+    //         <button className="button is-info" onClick={props.onEditClicked} disabled={!itemSelected}>
+    //             <span className="icon is-small">
+    //                 <FontAwesomeIcon icon={faEdit}/>
+    //             </span>
+    //         </button>
+    //         <button className="button is-danger" onClick={props.onDeleteClicked} disabled={!itemSelected}>
+    //             <span className="icon is-small">
+    //                 <FontAwesomeIcon icon={faTrash}/>
+    //             </span>
+    //         </button>
+    //     </div>
+    // </div>
+  );
+};
