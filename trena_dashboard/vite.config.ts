efb258a0e2fd993f4c77d0bfc60788840e47c94d
Filipe <filipeasm18@gmail.com>
2022-10-27 14:45:47 -0300

    Feat: Changed budle to Vite

---
+++ trena_dashboard/vite.config.ts
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ["decorators-legacy", "classProperties"],
+        },
+      },
+    }),
+    svgrPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+});
