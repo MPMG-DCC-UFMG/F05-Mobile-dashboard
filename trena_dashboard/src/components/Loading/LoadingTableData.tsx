2875b8e881c2d09b2995c6e9a8df9c213b999735
Filipe <filipeasm18@gmail.com>
2022-11-01 08:51:16 -0300

    Feat: New aux components

---
+++ trena_dashboard/src/components/Loading/LoadingTableData.tsx
@@ -0,0 +1,34 @@
+import { Box, Grid, LinearProgress, Paper } from "@mui/material";
+import React from "react";
+import { BreadCrumbSteps, Heading } from "../Heading";
+
+interface LoadingTableDataProps {
+  headingButtonTitle: string;
+  headingTitle: string;
+  headingSteps: BreadCrumbSteps[];
+  headingAction(): void;
+}
+
+export function LoadingTableData({
+  headingButtonTitle,
+  headingTitle,
+  headingSteps,
+  headingAction,
+}: LoadingTableDataProps) {
+  return (
+    <Grid style={{ width: "100%", marginTop: 14 }} item xs={12}>
+      <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+        <Heading
+          buttonTitle={headingButtonTitle}
+          title={headingTitle}
+          steps={headingSteps}
+          handleAction={headingAction}
+        >
+          <Box width="100%">
+            <LinearProgress />
+          </Box>
+        </Heading>
+      </Paper>
+    </Grid>
+  );
+}
---
+++ trena_dashboard/src/components/Snackbar/index.tsx
@@ -0,0 +1,35 @@
+import { Alert, Snackbar } from "@mui/material";
+import React from "react";
+
+interface MySnackbarProps {
+  setOpen(open: boolean): void;
+  open: boolean;
+  message: string;
+  severity: "error" | "warning" | "info" | "success";
+}
+
+export function MySnackbar({
+  open,
+  message,
+  severity,
+  setOpen,
+}: MySnackbarProps) {
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
