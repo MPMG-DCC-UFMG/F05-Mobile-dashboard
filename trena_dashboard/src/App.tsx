a16f7a2ea23b27191212852d44cd50ffbb8b25ce
Filipe <filipeasm18@gmail.com>
2022-11-01 08:51:02 -0300

    Fix: Configured QueryClient

--- trena_dashboard/src/App.tsx
+++ trena_dashboard/src/App.tsx
@@ -8,7 +8,18 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { AppRoutes } from "./routes/routes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 1000,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
