058b12a760b628797be02bffda52b9442e9dc1b0
Filipe <filipeasm18@gmail.com>
2023-01-08 14:58:45 -0300

    Chore: Add Call and Message models and fix extension

---
+++ trena_dashboard/src/core/models/Call.ts
@@ -0,0 +1,9 @@
+export interface Call {
+  id: string;
+  admin_email: string;
+  user_email: string;
+  title: string;
+  created_at: number;
+  closed: boolean;
+  closed_at: number;
+}
---
+++ trena_dashboard/src/core/models/Message.ts
@@ -0,0 +1,9 @@
+export interface Message {
+  id: string;
+  sender_email: string;
+  receiver_email: string;
+  text: string;
+  timestamp: number;
+  call_id: string;
+  readed: boolean;
+}
