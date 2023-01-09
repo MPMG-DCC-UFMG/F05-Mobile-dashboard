e96d5f6c402935a917b984201ce946091e1cce06
Filipe <filipeasm18@gmail.com>
2023-01-09 15:46:32 -0300

    Feat: Message Service impl

---
+++ trena_dashboard/src/core/models/dto/SendMessageDTO.ts
@@ -0,0 +1,6 @@
+export interface SendMessageDTO {
+  sender_email: string;
+  receiver_email: string;
+  text: string;
+  call_id: string;
+}
---
+++ trena_dashboard/src/core/network/services/MessagesService.ts
@@ -0,0 +1,45 @@
+import Config from "../../../config/Config";
+import { SendMessageDTO } from "../../models/dto/SendMessageDTO";
+import { Message } from "../../models/Message";
+import TrenaAPI from "../TrenaAPI";
+
+const baseUrl = Config.BASE_URL + "/message";
+
+const getMessagesFromCall = async (callId: string): Promise<Message[]> => {
+  const url = baseUrl;
+  const res = await TrenaAPI.network().query({ call_id: callId }).get(url);
+
+  return res.body;
+};
+
+const sendMessage = async (message: SendMessageDTO): Promise<Message> => {
+  const url = baseUrl;
+  const res = await TrenaAPI.network().post(url).send(message);
+
+  return res.body;
+};
+
+const deleteMessage = async (messageId: string): Promise<Message> => {
+  const url = baseUrl;
+  const res = await TrenaAPI.network()
+    .delete(url)
+    .query({ message_id: messageId });
+
+  return res.body;
+};
+
+const markMessageAsReaded = async (messageId: string): Promise<Message> => {
+  const url = baseUrl + "/read";
+  const res = await TrenaAPI.network()
+    .post(url)
+    .query({ message_id: messageId });
+
+  return res.body;
+};
+
+export const MessageServiceQuery = {
+  getMessagesFromCall,
+  sendMessage,
+  deleteMessage,
+  markMessageAsReaded,
+};
