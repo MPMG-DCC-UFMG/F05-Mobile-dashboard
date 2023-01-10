0056a36dc9a584735804eea7282ac1d16546a80c
George <george.fons@gmail.com>
2023-01-10 14:28:09 -0300

    Config ts

--- trena_dashboard/src/config/Config.tsx
+++
@@ -1,6 +0,0 @@
-export default class Config {
-  static ENVIRONMENT?: string = import.meta.env.VITE_REACT_APP_ENVIRONMENT;
-  static BASE_URL?: string = import.meta.env.VITE_REACT_APP_BASE_URL;
-  static SENTRY_DNS?: string = import.meta.env.VITE_REACT_APP_SENTRY_DNS;
-  static API_KEY?: string = import.meta.env.VITE_REACT_APP_API_KEY;
-}
