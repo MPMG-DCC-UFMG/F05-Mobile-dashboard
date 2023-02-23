c998f848c5ad2dea32f1aaf588d84c3e63dd5e47
Marco Tulio Mendes Madeira <tuliomarco9704@hotmail.com>
2023-02-23 15:01:13 -0300

    Refactor: Refact edit and add type work process

--- trena_dashboard/src/components/Dialogs/TypeOfWork/AddTypeOfWorkDialog.tsx
+++ trena_dashboard/src/components/Dialogs/TypeOfWork/AddTypeOfWorkDialog.tsx
@@ -1,26 +1,30 @@
 import {
-  Button,
-  Grid,
-  Step,
-  StepButton,
-  Stepper,
-  TextField,
-  Table,
-  TableHead,
-  TableCell,
-  TableBody,
-  TableRow,
-  Checkbox,
+	Button,
+	Grid,
+	Step,
+	StepButton,
+	Stepper,
+	TextField,
+	Table,
+	TableHead,
+	TableCell,
+	TableBody,
+	TableRow,
+	Checkbox,
 } from "@mui/material";
 import React, { useState } from "react";
-import {CreateTypeWorkDTO} from "../../../core/models/dto/typeWork/CreateTypeWorkDTO";
+import { CreateTypeWorkDTO } from "../../../core/models/dto/typeWork/CreateTypeWorkDTO";
 import {
 	SingleDialogContainer,
 	SingleDialogContainerProps,
 } from "../DialogContainer";
 import { useLoadTypePhotos } from "../../../core/network/queries/typePhotos/queries";
 import { useLoadWorkStatus } from "../../../core/network/queries/workStatus/queries";
-import { useAddTypeWork, useUpdateTypeWorkPhotos, useUpdateTypeWorkStatus } from "../../../core/network/queries/typeWork/mutations";
+import {
+	useAddTypeWork,
+	useUpdateTypeWorkPhotos,
+	useUpdateTypeWorkStatus,
+} from "../../../core/network/queries/typeWork/mutations";
 import { UpdateParams } from "../../../core/network/services/TypeWorkService";
 
 export function AddTypeOfWorkDialog({
@@ -28,176 +32,188 @@ export function AddTypeOfWorkDialog({
 	setState,
 	title,
 }: SingleDialogContainerProps) {
-  
-  const { data: typePhotos} = useLoadTypePhotos(); 
-  const { data: workStatus } = useLoadWorkStatus();
-  const {mutate: addTypeWork} = useAddTypeWork();
-  const {mutate: updateTypeWorkPhotos} = useUpdateTypeWorkPhotos();
-  const {mutate: updateTypeWorkStatus} = useUpdateTypeWorkStatus();
+	const { data: typePhotos } = useLoadTypePhotos();
+	const { data: workStatus } = useLoadWorkStatus();
+	const { mutate: addTypeWork } = useAddTypeWork();
+	const { mutate: updateTypeWorkPhotos } = useUpdateTypeWorkPhotos();
+	const { mutate: updateTypeWorkStatus } = useUpdateTypeWorkStatus();
 
-  const [typeWorkName, setTypeWorkName] = useState<string>("");
-  const [photoFlags, setPhotoFlags] =useState<number[]>([]);
-  const [workStatusFlags, setWorkStatusFlags] =useState<number[]>([]);  
-  const [completed, setCompleted] = useState<{
-    [k: number]: boolean;
-  }>({});
-  const [activeStep, setActiveStep] = useState(0);
-  const steps = ["Tipo de Obra", "Tipo de Foto", "Estado da Obra"];
+	const [typeWorkName, setTypeWorkName] = useState<string>("");
+	const [photoFlags, setPhotoFlags] = useState<number[]>([]);
+	const [workStatusFlags, setWorkStatusFlags] = useState<number[]>([]);
+	const [completed, setCompleted] = useState<{
+		[k: number]: boolean;
+	}>({});
+	const [activeStep, setActiveStep] = useState(0);
+	const steps = ["Tipo de Obra", "Tipo de Foto", "Estado da Obra"];
 
-  const handleStepChange = (step: number) => setActiveStep(step);
+	const handleStepChange = (step: number) => setActiveStep(step);
 
-  const handleAddTypeOfWork = () => {
-    addTypeWork({name: typeWorkName, status_list: []  } as CreateTypeWorkDTO, 
-      {onSuccess(data) {
-          updateTypeWorkPhotos({updateFlags: photoFlags, typeWorkFlag: data.flag!} as UpdateParams);
-          updateTypeWorkStatus({updateFlags: workStatusFlags, typeWorkFlag: data.flag!} as UpdateParams);
-      },}
-  );
-    handleCloseDialog();
-  };
+	const handleAddTypeOfWork = () => {
+		addTypeWork({ name: typeWorkName, status_list: [] } as CreateTypeWorkDTO, {
+			onSuccess(data) {
+				if (photoFlags.length !== 0) {
+					updateTypeWorkPhotos({
+						updateFlags: photoFlags,
+						typeWorkFlag: data.flag!,
+					} as UpdateParams);
+				}
+				if (workStatusFlags.length !== 0) {
+					updateTypeWorkStatus({
+						updateFlags: workStatusFlags,
+						typeWorkFlag: data.flag!,
+					} as UpdateParams);
+				}
+			},
+		});
+		handleCloseDialog();
+	};
 
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-  };
-  
-  const handleBack = () => {
-   setActiveStep(activeStep - 1);
-  };
-  
-  const handleCloseDialog = () => {
-    setState(false);
-    setActiveStep(0);
-    setTypeWorkName("");
-    setPhotoFlags([]);
-  };
+	const handleNext = () => {
+		setActiveStep(activeStep + 1);
+	};
 
-  const handlePhotoCheck = (flag: number, value: string) => {
-    if(value === "on"){
-      setPhotoFlags([...photoFlags, flag]);
-    }
-    else{
-      setPhotoFlags(photoFlags.filter((value)=> value !== flag));
-    }
-  }
+	const handleBack = () => {
+		setActiveStep(activeStep - 1);
+	};
 
-  const handleWorkStatusCheck = (flag: number, value: string) => {
-    if(value === "on"){
-      setWorkStatusFlags([...workStatusFlags, flag]);
-    }
-    else{
-      setWorkStatusFlags(workStatusFlags.filter((value)=> value !== flag));
-    }
-  }
+	const handleCloseDialog = () => {
+		setState(false);
+		setActiveStep(0);
+		setTypeWorkName("");
+		setPhotoFlags([]);
+		setWorkStatusFlags([]);
+	};
 
-  return (
-    <SingleDialogContainer state={state} setState={setState} title={title}>
-      <Grid item>
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => (
-            <Step key={label} completed={completed[index]}>
-              <StepButton onClick={() => handleStepChange(index)}>
-                {label}
-              </StepButton>
-            </Step>
-          ))}
-        </Stepper>
-      </Grid>
+	const handlePhotoCheck = (flag: number, value: string) => {
+		if (value === "on") {
+			setPhotoFlags([...photoFlags, flag]);
+		} else {
+			setPhotoFlags(photoFlags.filter((value) => value !== flag));
+		}
+	};
 
-      {activeStep === 0 && (
-        <Grid item sx={{ pt: 3 }}>
-          <TextField
-            onChange={(e) => setTypeWorkName(e.target.value)}
-            defaultValue={typeWorkName}
-            required
-            label="Tipo de Obra"
-            fullWidth
-          />
-        </Grid>
-      )}
-      {activeStep === 1 && (
-        <Grid item sx={{ pt: 3 }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Check</TableCell>
-                <TableCell>Tipo de Foto</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {typePhotos?.map((value) => (
-                <TableRow key={value.flag}>
-                  <TableCell>
-                    <Checkbox 
-                      onChange={(e)=> handlePhotoCheck(value.flag, e.target.value )}
-                    />
-                  </TableCell>
-                  <TableCell>{value.name}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Grid>
-      )}
-      {activeStep === 2 && (
-        <Grid item sx={{ pt: 3 }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Check</TableCell>
-                <TableCell>Estado da Obra</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {workStatus?.map((value, index) => (
-                <TableRow key={value.flag}>
-                  <TableCell>
-                    <Checkbox 
-                      onChange={(e)=> handleWorkStatusCheck(value.flag, e.target.value)}
-                    />
-                  </TableCell>
-                  <TableCell>{value.name}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Grid>
-      )}
-      <Grid
-        container
-        spacing={2}
-        sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}
-      >
-        <Grid item display="flex">
-          <Button onClick={handleCloseDialog} color="error" variant="contained">
-            Cancelar
-          </Button>
-        </Grid>
-        {activeStep > 0 && (
-          <Grid item display="flex">
-            <Button onClick={handleBack} color="info" variant="contained">
-              Anterior
-            </Button>
-          </Grid>
-        )}
-        {activeStep < 2 && (
-          <Grid item display="flex">
-            <Button onClick={handleNext} color="info" variant="contained">
-              Proximo
-            </Button>
-          </Grid>
-        )}
-        {activeStep === 2 && (
-          <Grid item display="flex">
-            <Button
-              onClick={handleAddTypeOfWork}
-              color="success"
-              variant="contained"
-            >
-              Confirmar
-            </Button>
-          </Grid>
-        )}
-      </Grid>
-    </SingleDialogContainer>
-  );
+	const handleWorkStatusCheck = (flag: number, value: string) => {
+		if (value === "on") {
+			setWorkStatusFlags([...workStatusFlags, flag]);
+		} else {
+			setWorkStatusFlags(workStatusFlags.filter((value) => value !== flag));
+		}
+	};
+
+	return (
+		<SingleDialogContainer state={state} setState={setState} title={title}>
+			<Grid item>
+				<Stepper activeStep={activeStep}>
+					{steps.map((label, index) => (
+						<Step key={label} completed={completed[index]}>
+							<StepButton onClick={() => handleStepChange(index)}>
+								{label}
+							</StepButton>
+						</Step>
+					))}
+				</Stepper>
+			</Grid>
+
+			{activeStep === 0 && (
+				<Grid item sx={{ pt: 3 }}>
+					<TextField
+						onChange={(e) => setTypeWorkName(e.target.value)}
+						defaultValue={typeWorkName}
+						required
+						label="Tipo de Obra"
+						fullWidth
+					/>
+				</Grid>
+			)}
+			{activeStep === 1 && (
+				<Grid item sx={{ pt: 3 }}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<TableCell>Check</TableCell>
+								<TableCell>Tipo de Foto</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{typePhotos?.map((value) => (
+								<TableRow key={value.flag}>
+									<TableCell>
+										<Checkbox
+											onChange={(e) =>
+												handlePhotoCheck(value.flag, e.target.value)
+											}
+										/>
+									</TableCell>
+									<TableCell>{value.name}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</Grid>
+			)}
+			{activeStep === 2 && (
+				<Grid item sx={{ pt: 3 }}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<TableCell>Check</TableCell>
+								<TableCell>Estado da Obra</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{workStatus?.map((value, index) => (
+								<TableRow key={value.flag}>
+									<TableCell>
+										<Checkbox
+											onChange={(e) =>
+												handleWorkStatusCheck(value.flag, e.target.value)
+											}
+										/>
+									</TableCell>
+									<TableCell>{value.name}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</Grid>
+			)}
+			<Grid
+				container
+				spacing={2}
+				sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}
+			>
+				<Grid item display="flex">
+					<Button onClick={handleCloseDialog} color="error" variant="contained">
+						Cancelar
+					</Button>
+				</Grid>
+				{activeStep > 0 && (
+					<Grid item display="flex">
+						<Button onClick={handleBack} color="info" variant="contained">
+							Anterior
+						</Button>
+					</Grid>
+				)}
+				{activeStep < 2 && (
+					<Grid item display="flex">
+						<Button onClick={handleNext} color="info" variant="contained">
+							Proximo
+						</Button>
+					</Grid>
+				)}
+				{activeStep === 2 && (
+					<Grid item display="flex">
+						<Button
+							onClick={handleAddTypeOfWork}
+							color="success"
+							variant="contained"
+						>
+							Confirmar
+						</Button>
+					</Grid>
+				)}
+			</Grid>
+		</SingleDialogContainer>
+	);
 }
--- trena_dashboard/src/components/Dialogs/TypeOfWork/EditTypeOfWorkDialog.tsx
+++ trena_dashboard/src/components/Dialogs/TypeOfWork/EditTypeOfWorkDialog.tsx
@@ -3,19 +3,27 @@ import {
 	Checkbox,
 	CircularProgress,
 	Grid,
+	Step,
+	StepButton,
+	Stepper,
 	Table,
+	TableBody,
 	TableCell,
-	TableContainer,
+	TableHead,
 	TableRow,
 	TextField,
 } from "@mui/material";
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { UpdateTypeWorkDTO } from "../../../core/models/dto/typeWork/UpdateTypeWorkDTO";
 import { TypeWork } from "../../../core/models/TypeWork";
 import { useLoadTypePhotos } from "../../../core/network/queries/typePhotos/queries";
-import { useUpdateTypeWork } from "../../../core/network/queries/typeWork/mutations";
-import { useTypePhotoStore } from "../../../core/store/typePhoto";
-import { closeDialog } from "../../../utils/dialogHandler";
+import {
+	useUpdateTypeWork,
+	useUpdateTypeWorkPhotos,
+	useUpdateTypeWorkStatus,
+} from "../../../core/network/queries/typeWork/mutations";
+import { useLoadWorkStatus } from "../../../core/network/queries/workStatus/queries";
+import { UpdateParams } from "../../../core/network/services/TypeWorkService";
 import { TableDialogContainer } from "../DialogContainer";
 
 interface EditTypeOfWorkDialog {
@@ -33,28 +41,76 @@ export function EditTypeOfWorkDialog({
 	index,
 	title,
 }: EditTypeOfWorkDialog) {
-	useLoadTypePhotos();
-	const typePhotos = useTypePhotoStore((state) => state.typePhotos);
-	const { mutate, isLoading } = useUpdateTypeWork();
-  const [typeWorkName, setTypeWorkName] = useState<string>(typeWork.name)
-	const [updateTypeWork, setUpdateTypeWork] = useState<UpdateTypeWorkDTO>({
-		name: "",
-		status_list: [],
-	});
+	const { data: typePhotos } = useLoadTypePhotos();
+	const { data: workStatus } = useLoadWorkStatus();
+	const { mutate: updateTypeWork, isLoading } = useUpdateTypeWork();
+	const { mutate: updateTypeWorkPhotos } = useUpdateTypeWorkPhotos();
+	const { mutate: updateTypeWorkStatus } = useUpdateTypeWorkStatus();
 
-	const handleCloseDialog = useCallback(() => {
-		closeDialog(state, setState, index);
-	}, []);
+	const [typeWorkName, setTypeWorkName] = useState<string>(typeWork.name);
+	const [photoFlags, setPhotoFlags] = useState<number[]>([]);
+	const [workStatusFlags, setWorkStatusFlags] = useState<number[]>([]);
+	const [completed, setCompleted] = useState<{
+		[k: number]: boolean;
+	}>({});
+	const [activeStep, setActiveStep] = useState(0);
+	const steps = ["Tipo de Obra", "Tipo de Foto", "Estado da Obra"];
 
-  const handleEditTypeOfWork = (typeWork: TypeWork) => {
-    typeWork.name = typeWorkName;
-    // editTypeWork(typeWork);
-    // handleCloseDialog(index);
-  };
-	const handleTypeOfWorkStatus = useCallback(() => {
-		mutate(updateTypeWork);
-		handleCloseDialog();
-	}, []);
+	const handleStepChange = (step: number) => setActiveStep(step);
+
+	const handleCloseDialog = () => {
+		setState(state.map((value) => (value === true ? false : value)));
+		setActiveStep(0);
+		setTypeWorkName(typeWork.name);
+		setPhotoFlags([]);
+		setWorkStatusFlags([]);
+	};
+
+	const handleUpdateTypeWork = () => {
+		updateTypeWork(
+			{ flag: typeWork.flag, name: typeWorkName, status_list: [] } as UpdateTypeWorkDTO,
+			{
+				onSuccess(data) {
+					if (photoFlags.length !== 0) {
+						updateTypeWorkPhotos({
+							updateFlags: photoFlags,
+							typeWorkFlag: data.flag!,
+						} as UpdateParams);
+					}
+					if (workStatusFlags.length !== 0) {
+						updateTypeWorkStatus({
+							updateFlags: workStatusFlags,
+							typeWorkFlag: data.flag!,
+						} as UpdateParams);
+					}
+				},
+			}
+		);
+	};
+
+	const handleNext = () => {
+		setActiveStep(activeStep + 1);
+	};
+
+	const handleBack = () => {
+		setActiveStep(activeStep - 1);
+	};
+
+	const handlePhotoCheck = (flag: number, value: string) => {
+		if (value === "on") {
+			setPhotoFlags([...photoFlags, flag]);
+		} else {
+			setPhotoFlags(photoFlags.filter((value) => value !== flag));
+		}
+	};
+
+	const handleWorkStatusCheck = (flag: number, value: string) => {
+		if (value === "on") {
+			setWorkStatusFlags([...workStatusFlags, flag]);
+		} else {
+			setWorkStatusFlags(workStatusFlags.filter((value) => value !== flag));
+		}
+	};
 
 	return (
 		<TableDialogContainer
@@ -63,54 +119,116 @@ export function EditTypeOfWorkDialog({
 			index={index}
 			title={title}
 		>
-			<Grid container justifyContent="space-between" alignItems="center">
-				<TextField
-					onChange={(e) =>
-						setUpdateTypeWork({ ...updateTypeWork, name: e.target.value })
-					}
-					required
-					label="Tipo da Obra"
-					defaultValue={typeWork.name}
-					fullWidth
-				/>
-				<TableContainer>
+			<Grid item>
+				<Stepper activeStep={activeStep}>
+					{steps.map((label, index) => (
+						<Step key={label} completed={completed[index]}>
+							<StepButton onClick={() => handleStepChange(index)}>
+								{label}
+							</StepButton>
+						</Step>
+					))}
+				</Stepper>
+			</Grid>
+			{activeStep === 0 && (
+				<Grid item sx={{ pt: 3 }}>
+					<TextField
+						onChange={(e) => setTypeWorkName(e.target.value)}
+						defaultValue={typeWorkName}
+						required
+						label="Tipo de Obra"
+						fullWidth
+					/>
+				</Grid>
+			)}
+			{activeStep === 1 && (
+				<Grid item sx={{ pt: 3 }}>
 					<Table>
-						{typePhotos.map((typePhoto) => (
-							<TableRow key={typePhoto.flag}>
-								<TableCell>
-									<Checkbox />
-								</TableCell>
-								<TableCell>{typePhoto.name}</TableCell>
+						<TableHead>
+							<TableRow>
+								<TableCell>Check</TableCell>
+								<TableCell>Tipo de Foto</TableCell>
 							</TableRow>
-						))}
+						</TableHead>
+						<TableBody>
+							{typePhotos?.map((value) => (
+								<TableRow key={value.flag}>
+									<TableCell>
+										<Checkbox
+											onChange={(e) =>
+												handlePhotoCheck(value.flag, e.target.value)
+											}
+										/>
+									</TableCell>
+									<TableCell>{value.name}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
 					</Table>
-				</TableContainer>
-				<Grid
-					container
-					spacing={2}
-					sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}
-				>
+				</Grid>
+			)}
+			{activeStep === 2 && (
+				<Grid item sx={{ pt: 3 }}>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<TableCell>Check</TableCell>
+								<TableCell>Estado da Obra</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{workStatus?.map((value, index) => (
+								<TableRow key={value.flag}>
+									<TableCell>
+										<Checkbox
+											onChange={(e) =>
+												handleWorkStatusCheck(value.flag, e.target.value)
+											}
+										/>
+									</TableCell>
+									<TableCell>{value.name}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</Grid>
+			)}
+			<Grid
+				container
+				spacing={2}
+				sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}
+			>
+				<Grid item display="flex">
+					<Button onClick={handleCloseDialog} color="error" variant="contained">
+						Cancelar
+					</Button>
+				</Grid>
+				{activeStep > 0 && (
 					<Grid item display="flex">
-						<Button
-							onClick={() => handleCloseDialog()}
-							color="error"
-							variant="contained"
-						>
-							Cancelar
+						<Button onClick={handleBack} color="info" variant="contained">
+							Anterior
 						</Button>
 					</Grid>
+				)}
+				{activeStep < 2 && (
+					<Grid item display="flex">
+						<Button onClick={handleNext} color="info" variant="contained">
+							Proximo
+						</Button>
+					</Grid>
+				)}
+				{activeStep === 2 && (
 					<Grid item display="flex">
 						<Button
-							onClick={() => handleTypeOfWorkStatus()}
+							onClick={() => handleUpdateTypeWork()}
 							color="success"
 							variant="contained"
 						>
 							{isLoading ? <CircularProgress /> : "Salvar"}
 						</Button>
 					</Grid>
-				</Grid>
+				)}
 			</Grid>
 		</TableDialogContainer>
 	);
 }
-
